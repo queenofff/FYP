8f7ca9fc10a24bda830496ad65e6121361775937
SLRU0 <119642591+SLRU0@users.noreply.github.com>
2023-06-23 20:50:30 +0800

    Start Scene

---
+++ FYP/Assets/Creator Kit - RPG/Scripts/UI/Exit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+}

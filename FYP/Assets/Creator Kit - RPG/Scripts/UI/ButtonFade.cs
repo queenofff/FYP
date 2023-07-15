445ca26cb61ac3aa7fcef3dde8808e7ee9d351fe
SLRU0 <119642591+SLRU0@users.noreply.github.com>
2023-07-15 23:16:51 +0800

    fade transform position

--- FYP/Assets/Creator Kit - RPG/Scripts/UI/ButtonFade.cs
+++ FYP/Assets/Creator Kit - RPG/Scripts/UI/ButtonFade.cs
@@ -6,10 +6,14 @@ public class ButtonFade : MonoBehaviour
 {
     [SerializeField] CanvasGroup starMenu;
     [SerializeField] GameObject UI;
+    [SerializeField] private float speed = 1;
+    [SerializeField] RectTransform rectTransform;
+    
 
     private float fadeEndTime = 0.0f;
     private float fadeStartTime = 1.0f;
     private float fadeTimer = 1.0f;
+    
 
     private bool fadeIn = false;
     private float maths = 0.0f;
@@ -25,6 +29,9 @@ public class ButtonFade : MonoBehaviour
     {
         if (fadeIn != false)
         {
+            Vector2 targetPosition = new Vector2(440f, 0f);
+            rectTransform.anchoredPosition = Vector2.Lerp(
+                rectTransform.anchoredPosition, targetPosition, speed * Time.deltaTime);
             fadeTimer -= Time.deltaTime;
             starMenu.alpha = fadeTimer;
         }
